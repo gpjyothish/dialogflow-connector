78fdc0ae5904eb6fd30483e5f4b9bf31ed5ec009
Jyothish <gpjyothish@gmail.com>
2020-11-27 23:21:59 +0530

    dialogflow cx library integration

--- src/connector.js
+++ src/connector.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var http = require('http')
+const {SessionsClient} = require('@google-cloud/dialogflow-cx')
+const client = new SessionsClient()
 
 const { Facebook } = require('./channels/facebook')
 
@@ -32,9 +34,10 @@ class Connector {
      */
     configureWebhookEndpoint() {
         if (this.webserver) {
-            this.webserver.post(this.config.webhook_uri, (req, res) => {
+            this.webserver.post(this.config.webhook_uri, async (req, res) => {
                 res.send('ok')
-                console.log(req.body)
+                console.log(JSON.stringify(req.body))
+                var result = await this.detectIntentText()
             })
         }
         else {
@@ -46,6 +49,44 @@ class Connector {
         if (this.config.chennels.facebook) {
             const FacebookConnector = new Facebook(this.config, this.webserver)
         }
+    }
+
+    async detectIntentText() {
+        const projectId = process.env.DIALOGFLOW_PROJECT_ID
+        const location = process.env.DIALOGFLOW_AGENT_LOCATION
+        const agentId = process.env.DIALOGFLOW_AGENT_ID
+        const query = ['start']
+        const languageCode = 'en'
+
+        const sessionId = Math.random().toString(36).substring(7)
+        const sessionPath = client.projectLocationAgentSessionPath(
+            projectId,
+            location,
+            agentId,
+            sessionId
+        )
+        const request = {
+            session: sessionPath,
+            queryInput: {
+              text: {
+                    text: query
+                },
+                languageCode
+            }
+        }
+
+        const [response] = await client.detectIntent(request)
+        console.log(`User Query: ${query}`)
+
+        for (const message of response.queryResult.responseMessages) {
+            if (message.text) {
+                console.log(`Agent Response: ${message.text.text}`)
+            }
+        }
+        if (response.queryResult.match.intent) {
+            console.log(`Matched Intent: ${response.queryResult.match.intent.displayName}`)
+        }
+        console.log(`Current Page: ${response.queryResult.currentPage.displayName}`)
     }
 }
 exports.Connector = Connector
